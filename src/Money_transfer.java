815da1e82806cc921f440beb4ab59dbeab758c27
Anton Usoltsev <usoltsevjr@gmail.com>
2020-07-07 14:24:45 +0300

    First commit

---
+++ src/Money_transfer.java
@@ -0,0 +1,8 @@
+public class Money_transfer {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total_balance = balance + transfer;
+        System.out.println("Баланс после перевода = " + total_balance);
+    }
+}
